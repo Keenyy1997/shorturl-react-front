b82104b68fcc67d26141ee66af9deceb34570755
kennyvallejoc <keenyy1997@gmail.com>
2020-07-04 19:17:58 -0500

    Modify `process.env.REACT_BASE_URL` for `BASE_URL`

--- src/integrations/api.js
+++ src/integrations/api.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_BASE_URL || 'https://shorturl-ts.herokuapp.com';
+export const BASE_URL = 'https://shorturl-ts.herokuapp.com';
 
 export async function PostRequest(url, body, headers = {}) {
   
